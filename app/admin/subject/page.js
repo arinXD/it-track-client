37fed3fa2f3602fe5d68828d819f0842a3dd1484
ArinSwagg <101942998+arinXD@users.noreply.github.com>
2023-12-13 06:34:41 +0700

    Subbranch (#4)
    * test review state change
    
    * test
    
    * test merge
    
    * add academic crud for admin
    
    * fix error
    
    ---------
    
    Co-authored-by: kiok127523 <kiok127523@gmail.com>

--- app/admin/subject/page.js
+++ app/admin/subject/page.js
@@ -1,8 +1,7 @@
 
 // Subject.js
 // import React, { useState, useEffect } from 'react';
-import { Navbar, Sidebar,ExcelUpload } from '@/app/components';
-import { TablePagination } from "../components";
+import { Navbar, Sidebar,ExcelUpload, TablePagination } from '@/app/components';
 import axios from "axios";
 import { hostname } from '@/app/api/hostname'
 
@@ -22,7 +21,6 @@ async function fetchData() {
 
 export default async function Subject() {
     const data = await fetchData()
-    console.log(data);
     return (
         <>
             <header>
@@ -35,7 +33,6 @@ export default async function Subject() {
                     <div>
                         <TablePagination data={data} />
                         <ExcelUpload />
-                        <p>test</p>
                     </div>
                 </div>
             </div>
--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,6 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import CredentialsProvider from "next-auth/providers/credentials";
 import axios from "axios";
-import { NextResponse } from 'next/server'
 import { hostname } from '@/app/api/hostname'
 
 const VerifiedEmailProvider = (options) => {
