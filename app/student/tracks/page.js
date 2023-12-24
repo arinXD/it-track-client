242ec426837db6270696fdc8ea8e83aaff84c5c1
ArinSwagg <rakuzanoat@gmail.com>
2023-12-25 04:28:56 +0700

    try to fix error.

--- app/student/tracks/page.js
+++ app/student/tracks/page.js
@@ -1,11 +1,29 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import { Navbar, Sidebar, ContentWrap } from '@/app/components';
+import { useSession } from "next-auth/react"
 import TrackSelectionForm from './TrackSelectionForm';
 import StudentEnrollment from './StudentEnrollment';
 const Page = () => {
     const [userData, setUserData] = useState({})
     const [enrollments, setEnrollment] = useState([])
+    const { data: session } = useSession();
+    console.log(session);
+    if (!session?.user?.stu_id) {
+        return (
+            <>
+                <header>
+                    <Navbar />
+                </header>
+                <Sidebar />
+                <ContentWrap>
+                    <div>
+                        Sign in as kkumail.
+                    </div>
+                </ContentWrap>
+            </>
+        )
+    }
     // if (!(Object.keys(userData).length === 0)) {
     //     console.log(enrollments);
     // }
