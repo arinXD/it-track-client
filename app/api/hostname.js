8752131250bc227cc02f1a9e9d0e0a1085d984f3
ArinSwagg <rakuzanoat@gmail.com>
2024-05-08 02:26:11 +0700

    fix error

--- app/api/hostname.js
+++ app/api/hostname.js
@@ -2,8 +2,7 @@ const hostNameStage = {
     "dev": "http://localhost:4000",
     "prod": "https://it-track.arinchawut.com"
 }
-const hostname = hostNameStage[process.env.STAGE] || hostNameStage["dev"]
-
+const hostname = hostNameStage[process.env.STAGE]
 module.exports = {
     hostname
 }
