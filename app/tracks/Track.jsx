719c228cb4f8a1174b52aa0a1f519f5c26a1acea
ArinSwagg <rakuzanoat@gmail.com>
2024-05-11 10:06:57 +0700

    track title

--- app/tracks/Track.jsx
+++ app/tracks/Track.jsx
@@ -64,10 +64,19 @@ const Track = ({ tracks }) => {
                                                 width={1000}
                                                 height={1000}
                                                 unoptimized="true"
-                                                className="z-0 w-full h-full object-cover"
+                                                className="z-0 w-full h-full object-cover brightness-75"
                                                 quality={100}
                                                 alt={track.track}
                                                 src={track.img} />
+                                            <p
+                                                style={{
+                                                    top: "50%",
+                                                    left: "50%",
+                                                    transform: "translate(-50%, -50%)"
+                                                }}
+                                                className='w-full font-bold text-white absolute uppercase px-10 text-xl text-center'>
+                                                {track.track}
+                                            </p>
                                             <CardFooter className="flex-col justify-center h-full text-white bg-opacity-60 bg-black text-small absolute bottom-[-30em] group-hover:bottom-0 ease-in-out duration-400">
                                                 <span className='text-3xl font-extrabold'>{track.track}</span>
                                                 <hr className='w-10 mt-7 mb-5 border-2 border-gray-500'></hr>
@@ -78,54 +87,6 @@ const Track = ({ tracks }) => {
                                 );
                             })}
                         </div>
-                        {/* <ul className='space-y-6'>
-                            {tracks.map((track, index) => (
-                                <li key={index}
-                                    className='flex flex-col sm:flex-row justify-start items-start gap-6 border-b-1 pb-4'>
-                                    <div
-                                        className='w-full sm:w-[250px] h-[180px] relative'>
-                                        <Image
-                                            priority={true}
-                                            width={600}
-                                            height={400}
-                                            className='w-full h-full object-cover brightness-75'
-                                            alt={track.track}
-                                            src={track.img} />
-                                        <p
-                                            style={{
-                                                top: "50%",
-                                                left: "50%",
-                                                transform: "translate(-50%, -50%)"
-                                            }}
-                                            className='w-full font-bold text-white absolute uppercase px-10 text-xl text-center'>
-                                            {track.track}
-                                        </p>
-                                    </div>
-                                    <div className='flex flex-col items-start justify-between w-full h-full'>
-                                        <div className='flex flex-col gap-4'>
-                                            <div className='flex flex-col gap-1'>
-                                                <p className='font-bold text-lg'>{track.title_en}</p>
-                                                <p>{track.title_th}</p>
-                                            </div>
-                                            <p className='text-default-500'>
-                                                {track.desc}
-                                            </p>
-                                        </div>
-                                        <Link
-                                            href={`/tracks/${track.track?.toLowerCase()}`}
-                                            className='flex w-full md:w-fit sm:justify-start mt-4'>
-                                            <Button
-                                                radius='sm'
-                                                className='!w-full'
-                                                color={"primary"}
-                                            >
-                                                รายละเอียด
-                                            </Button>
-                                        </Link>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul> */}
                     </div >
             }
         </>
