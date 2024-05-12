a4501fd30fa29e747c878c0e26e4e582bd74347d
ArinSwagg <rakuzanoat@gmail.com>
2024-05-12 10:52:06 +0700

    handle credentials login

--- app/components/VerifyLogin.jsx
+++ app/components/VerifyLogin.jsx
@@ -20,7 +20,6 @@ const VerifyLogin = ({ verifyData }) => {
         const setVerifyData = async () => {
             if (verifyData.userData) {
                 const { email } = verifyData.userData
-                console.log("verify child", verifyData.userData);
                 setTimeout(async () => {
                     await signIn("verifiedEmail", {
                         email,
@@ -29,23 +28,13 @@ const VerifyLogin = ({ verifyData }) => {
                     })
                 }, 3000)
             }
-            else {
-                console.log("error in verify email");
-                console.log(session);
-            }
         }
         setVerifyData()
     }, [])
 
     return (
-        <div>
-            {/* <SignInButton /> */}
-            {/* <div className='my-3 flex gap-4'>
-                <button onClick={() => testLog()} className='btn btn-primary'>session</button>
-                <button onClick={() => updateSession()} className='btn btn-primary'>update</button>
-                <button onClick={() => getLocalStorage()} className='btn btn-primary'>get local storage</button>
-            </div> */}
-        </div>
+        <>
+        </>
     )
 }
 
