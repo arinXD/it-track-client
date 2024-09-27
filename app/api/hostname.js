79e0a3269841d0d83291a69be8106046c4f83194
arinxd <rakuzanoat@gmail.com>
2024-09-27 21:38:49 +0700

    change default staging.

--- app/api/hostname.js
+++ app/api/hostname.js
@@ -2,7 +2,7 @@ const hostNameStage = {
     "dev": "http://localhost:4000",
     "prod": "https://it-track.arinchawut.com"
 }
-let hostname = hostNameStage[process.env.STAGE] || hostNameStage.dev;
+let hostname = hostNameStage[process.env.STAGE] || hostNameStage.prod;
 module.exports = {
     hostname
 }
