ea16fb001e42fadeb8c48fae589a1d5012b4fe4a
nuttttx <rakuzanoat@gmail.com>
2024-10-27 18:19:30 +0700

    redesign fallback

--- app/components/SearchFallback.jsx
+++ app/components/SearchFallback.jsx
@@ -3,13 +3,20 @@ import { Progress } from "@nextui-org/react";
 
 const SearchFallback = () => {
      return (
-          <div className="w-full flex justify-center items-center">
+          <div className="w-full flex justify-center items-center mt-16">
                <Progress
                     size="sm"
-                    label="Loading..."
+                    label={
+                         <div className="w-full text-center">
+                              Loading...
+                         </div>
+                    }
                     isIndeterminate
                     aria-label="Loading..."
-                    className="max-w-md"
+                    className="max-w-md text-center"
+                    classNames={{
+                         label: "block w-full tracking-wider font-medium text-default-600",
+                    }}
                />
           </div>
      )
---
+++ app/test/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SearchFallback from '../components/SearchFallback'
+
+const page = () => {
+     return (
+          <SearchFallback />
+     )
+}
+
+export default page
