82cb1a4e609896558f04763e2d5ca160946e11e5
arinchawut <arinchawut.k@gli.co.th>
2025-06-04 00:09:34 +0700

    point to test stage

--- app/api/hostname.js
+++ app/api/hostname.js
@@ -1,19 +1,15 @@
 require("dotenv").config();
 
 const hostNameStage = {
-    dev: "http://localhost:4000",
-    prod: "https://it-track.arinchawut.com",
     test: "https://hazardous-thirty-addition-fraction.trycloudflare.com"
 };
 
 const getHostname = () => {
-    const stage = process.env.STAGE || "prod";
     return hostNameStage.test;
 };
 
 export const hostname = getHostname();
 
 export const getServerSideHostname = () => {
-    const stage = process.env.STAGE;
     return hostNameStage.test;
 };
