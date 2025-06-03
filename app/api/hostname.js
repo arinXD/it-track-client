427226a3b1f59223e2b174b95564e33e478eb69d
arinchawut <arinchawut.k@gli.co.th>
2025-06-04 00:04:48 +0700

    backend uri with cloudflare

--- app/api/hostname.js
+++ app/api/hostname.js
@@ -2,17 +2,18 @@ require("dotenv").config();
 
 const hostNameStage = {
     dev: "http://localhost:4000",
-    prod: "https://it-track.arinchawut.com"
+    prod: "https://it-track.arinchawut.com",
+    test: "https://hazardous-thirty-addition-fraction.trycloudflare.com"
 };
 
 const getHostname = () => {
     const stage = process.env.STAGE || "prod";
-    return hostNameStage[stage];
+    return hostNameStage.test;
 };
 
 export const hostname = getHostname();
 
 export const getServerSideHostname = () => {
     const stage = process.env.STAGE;
-    return hostNameStage[stage];
+    return hostNameStage.test;
 };
